df8ee6f93a1e5da5ca96e284fe6e3de02f256c9d
Winston Weinert <winston@ml1.net>
2014-01-24 22:20:29 -0600

    Added TutorialPageViewController — works nicely, now I just need to add content.

---
+++ TutorialPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  TutorialPageViewController.h
+//  Stronghold Locator
+//
+//  Created by Winston Weinert on 1/15/14.
+//  Copyright (c) 2014 Winston Weinert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialPageViewController : UIPageViewController <UIPageViewControllerDataSource>
+
+@property NSArray *pages;
+
+@end
